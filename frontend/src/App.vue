a7f27c936e759b39df1f85d84b33682eb1ae25f1
OHSONGMIN <dhthdals@gmail.com>
2024-05-23 15:22:32 +0900

    feat: App.vue 생성 및 import Home, Header, Footer.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header/>
+  <Home/>
+  <Footer/>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Home from "@/pages/Home.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Footer,
+    Home,
+    Header
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
