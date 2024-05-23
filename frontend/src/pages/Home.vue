77076f23ee5efe6734c458a9d6567e84fa8d3ca1
OHSONGMIN <dhthdals@gmail.com>
2024-05-23 15:52:43 +0900

    feat: import Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,10 +1,16 @@
 <template>
   This is App.vue~~~~~~~~~
+  <div class="col" v-for="i in 12" :key="i">
+    <Card/>
+  </div>
 </template>
 
 <script>
+import Card from "@/components/Card.vue";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {Card}
 }
 </script>
 
