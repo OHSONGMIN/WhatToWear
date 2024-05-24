1935739254391fbd1c5f6511f8c896edc20c3ecd
OHSONGMIN <dhthdals@gmail.com>
2024-05-24 13:42:24 +0900

    feat: import axios
    axios로 /api/items에서 데이터 가져오는 기능 추가

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,16 +1,29 @@
 <template>
   This is App.vue~~~~~~~~~
-  <div class="col" v-for="i in 12" :key="i">
-    <Card/>
+  <div class="col" v-for="(item, idx) in state.items" :key="idx">
+    <Card :item="item"/> <!-- item이라는 이름으로 item객체를 전달-->
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import {reactive} from "vue";
 import Card from "@/components/Card.vue";
 
 export default {
   name: "Home",
-  components: {Card}
+  components: {Card},
+  setup() {
+    const state = reactive({
+      items: []
+    })
+
+    axios.get("/api/items").then((res) => {
+      state.itmes = res.data;
+    })
+
+    return {state}
+  }
 }
 </script>
 
