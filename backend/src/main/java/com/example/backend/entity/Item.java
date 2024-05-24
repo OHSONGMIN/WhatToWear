dfa9fbbd4714c963009e5e5ffc0ed9d94f7c7c56
OHSONGMIN <dhthdals@gmail.com>
2024-05-24 13:42:50 +0900

    feat: Item Entity 생성

---
+++ backend/src/main/java/com/example/backend/entity/Item.java
@@ -0,0 +1,17 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+}
