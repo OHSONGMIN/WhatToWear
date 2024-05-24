d3494eb3c593dc8f406246bfc528cdcad30d2711
OHSONGMIN <dhthdals@gmail.com>
2024-05-24 16:38:44 +0900

    feat: store.js에 구현한 vuex를 main.js에서 사용하겠다며 선언

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import {createApp} from 'vue'
+import store from "@/scripts/store"
 import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router';
 import Home from "@/pages/Home.vue";
@@ -15,4 +16,4 @@ const router = createRouter({
     routes: routes
 })
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
