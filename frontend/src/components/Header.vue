5ffe916a5575a6ccdb7d83bbb99be403a548344a
OHSONGMIN <dhthdals@gmail.com>
2024-05-25 14:59:53 +0900

    feat: logout 기능

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,10 +1,32 @@
 <template>
-This is Header!!!
+  This is Header!!!
+  <div>
+    <li>
+      <router-link class="text-black" to="/">메인 화면</router-link>
+    </li>
+    <li>
+      <!-- store에 접근할 땐 $를 써서... -->
+      <router-link to="/login" class="text-black" v-if="!$store.state.account.id">로그인</router-link>
+      <a to="/login" class="text-black" @click="logout()" v-else>로그아웃</a>
+    </li>
+  </div>
 </template>
 
 <script>
+import store from "@/scripts/store";
+import router from "@/scripts/router";
+
 export default {
-  name: 'Header'
+  name: 'Header',
+  setup() {
+    const logout = () => {
+      store.commit("setAccount", 0);
+      sessionStorage.removeItem("id");
+      router.push({path: "/"});
+    }
+
+    return {logout}
+  }
 }
 </script>
 
