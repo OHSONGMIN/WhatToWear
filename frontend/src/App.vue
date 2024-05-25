1ee225121ce625d2394d8576abfdfb86e2c83d57
OHSONGMIN <dhthdals@gmail.com>
2024-05-25 15:38:06 +0900

    feat: sessionStorage에 id를 저장하는 방식 대신
    url이 바뀔 때마다(경로가 바뀔 때마다)
    check api를 요청해서 id 값을 넣어주거나 0으로 초기화하는 로직 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,9 @@
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import store from "@/scripts/store";
+import axios from "axios";
+import {useRoute} from "vue-router";
+import {watch} from "vue";
 
 export default {
   name: 'App',
@@ -16,11 +19,24 @@ export default {
     Header
   },
   setup() {
-    const id = sessionStorage.getItem("id");
+    const check = () => {
+      axios.get("/api/account/check").then((res) => {
+        console.log(res.data);
 
-    if (id) {
-      store.commit("setAccount", id);
-    }
+        if (res.data) {
+          store.commit("setAccount", res.data);
+        }
+        else {
+          store.commit("setAccount", 0);
+        }
+      })
+    };
+
+    const route = useRoute(); //url 관련된 정보를 가져오고
+
+    watch(route, () => { //경로가 바뀔 때마다 감시하는 watch
+      check(); // 즉... route가 바뀔 때마다 check 메서드를 실행하라
+    })
   }
 }
 </script>
