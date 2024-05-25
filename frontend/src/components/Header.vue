da732bf7ce621c6844995d9c308523c35db10afb
OHSONGMIN <dhthdals@gmail.com>
2024-05-26 02:18:24 +0900

    feat: 로그인 시 "주문 내역"이 보이도록 수정

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,6 +5,9 @@
       <li>
         <router-link class="text-black" to="/">메인 화면</router-link>
       </li>
+      <li v-if="$store.state.account.id">
+        <router-link to="/orders">주문 내역</router-link>
+      </li>
       <li>
         <!-- store에 접근할 땐 $를 써서... -->
         <router-link to="/login" class="text-black" v-if="!$store.state.account.id">로그인</router-link>
