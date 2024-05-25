b78aaad8ce5c30a58cd1b92bf61217d7f84eacca
OHSONGMIN <dhthdals@gmail.com>
2024-05-25 15:04:28 +0900

    feat: 로그인 요청이 와서 member값이 null이 아니면 id값을 token화 해서 Cookie에 넣은 다음에 응답 값에 추가

--- backend/src/main/java/com/example/backend/controller/AccountController.java
+++ backend/src/main/java/com/example/backend/controller/AccountController.java
@@ -2,10 +2,17 @@ package com.example.backend.controller;
 
 import com.example.backend.entity.Member;
 import com.example.backend.repository.MemberRepository;
+import com.example.backend.service.JwtService;
+import com.example.backend.service.JwtServiceImpl;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
@@ -17,14 +24,30 @@ public class AccountController {
     MemberRepository memberRepository;
 
     @PostMapping("/api/account/login")
-    public int login(@RequestBody Map<String, String> params) {
+    public ResponseEntity login(@RequestBody Map<String, String> params,
+                                HttpServletResponse res) {
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
 
-        if(member != null) {
-            return member.getId();
+        if (member != null) {
+            //return member.getId();
+            //JwtService 구현 후
+            JwtService jwtService = new JwtServiceImpl();
+            int id = member.getId();
+            String token = jwtService.getToken("id", id);
+
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true); //js로 접근할 수 없도록
+            cookie.setPath("/");
+
+            res.addCookie(cookie);
+
+            //return ResponseEntity.ok().build();
+            return new ResponseEntity<>(id, HttpStatus.OK);
+            //라고 하면 응답값으로 id를 줄 수 있음
         }
 
-        return 0;
+        //로그인 실패했을 때
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
 }
