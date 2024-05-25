107b6ca05d4636de2c06d6da5a0f18db6a43d6a4
OHSONGMIN <dhthdals@gmail.com>
2024-05-25 17:07:27 +0900

    feat: Enter키로 로그인 할 수 있도록 추가

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -4,21 +4,17 @@
 
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+             @keyup.enter="submit()"
              v-model="state.form.email">
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+             @keyup.enter="submit()"
              v-model="state.form.password">
       <label for="floatingPassword">Password</label>
     </div>
 
-    <div class="form-check text-start my-3">
-      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-      <label class="form-check-label" for="flexCheckDefault">
-        Remember me
-      </label>
-    </div>
     <button class="btn btn-primary w-100 py-2" @click="submit()">Sign in</button>
     <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
   </div>
