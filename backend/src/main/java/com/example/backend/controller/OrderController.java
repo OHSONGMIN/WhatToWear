6e5d6866f2d67e74589ccae6e6d9b65685ebd52f
OHSONGMIN <dhthdals@gmail.com>
2024-05-26 02:20:45 +0900

    feat: OrderController
    pushOrder(주문하기)
    getOrder(주문 내역 불러오기)

---
+++ backend/src/main/java/com/example/backend/controller/OrderController.java
@@ -0,0 +1,62 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.OrderDto;
+import com.example.backend.entity.Cart;
+import com.example.backend.entity.Item;
+import com.example.backend.entity.Order;
+import com.example.backend.repository.CartRepository;
+import com.example.backend.repository.ItemRepository;
+import com.example.backend.repository.OrderRepository;
+import com.example.backend.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    JwtService jwtService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping("/api/orders")
+    public ResponseEntity getORder (
+            @CookieValue(value = "token", required = false) String token
+    ) {
+        if (!jwtService.isValid(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        List<Order> orders = orderRepository.findAll();
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/api/orders")
+    public ResponseEntity pushOrder(
+            @RequestBody OrderDto dto,
+            @CookieValue(value = "token", required = false) String token
+    ) {
+        if (!jwtService.isValid(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        Order newOrder = new Order();
+        newOrder.setMemberId((jwtService.getId(token)));
+        newOrder.setName(dto.getName());
+        newOrder.setAddress(dto.getAddress());
+        newOrder.setPayment(dto.getPayment());
+        newOrder.setCardNumber(dto.getCardNumber());
+        newOrder.setItems(dto.getItems());
+
+        orderRepository.save(newOrder);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
