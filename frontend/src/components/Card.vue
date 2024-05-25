9ac20863d462f88a67105bf8f4ab653fd7a67e15
OHSONGMIN <dhthdals@gmail.com>
2024-05-26 00:54:41 +0900

    feat: Card.vue에 addToCart 기능 추가
    axios.get("/api/cart/items/${itemId}")

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,13 +1,27 @@
 <template>
   Card........
   {{item.name}}
+  <button class="btn btn-primary" @click="addToCart(item.id)">
+    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+  </button>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Card",
   props: {
     item: Object
+  },
+  setup() {
+    const addToCart = (itemdId) => {
+      axios.post(`/api/cart/items/${itemdId}`).then(() => {
+        console.log("success");
+      })
+    };
+
+    return {addToCart}
   }
 }
 </script>
