f1979321c5992208cbbb54912a99183fd29f9885
OHSONGMIN <dhthdals@gmail.com>
2024-05-25 15:47:12 +0900

    feat: JwtService의 getClaims 메서드 추가(JWT 토큰을 디코딩하고 서명을 검증하며, 만료 여부를 확인한 뒤, 토큰에 포함된 claims 반환)
    JwtServiceImpl에 @Service 어노테이션 추가

--- backend/src/main/java/com/example/backend/service/JwtServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/JwtServiceImpl.java
@@ -1,8 +1,7 @@
 package com.example.backend.service;
 
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
@@ -11,6 +10,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service("jwtService")
 public class JwtServiceImpl implements JwtService {
 
     private String secretKey = "asjdk!1256kdjlrk$#@fjdkli$fdskfjlwepcmv@#53345sjyt";
@@ -38,5 +38,24 @@ public class JwtServiceImpl implements JwtService {
                 .signWith(signKey, SignatureAlgorithm.HS256);
 
         return builder.compact();
+    }
+
+    @Override
+    public Claims getClaims(String token) {
+
+        if (token != null || !"".equals(token)) {
+            try {
+                String base64EncodedSecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+                byte[] secretByteKey = Base64.getDecoder().decode(base64EncodedSecretKey);
+                Key signKey = new SecretKeySpec(secretByteKey, SignatureAlgorithm.HS256.getJcaName());
+
+                return Jwts.parserBuilder().setSigningKey(signKey).build().parseClaimsJws(token).getBody();
+            } catch (ExpiredJwtException e) {
+                // 만료됨
+            } catch (JwtException e) {
+                // 유효하지 않음
+            }
+        }
+        return null;
     }
 }
