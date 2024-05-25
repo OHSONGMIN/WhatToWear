84038de0f1844e3896b53f4ae200cd33baf409f5
OHSONGMIN <dhthdals@gmail.com>
2024-05-25 15:05:30 +0900

    feat: JwtService
    JWT 토큰 생성 기능을 제공하며
    Base64 디코딩을 통해 secretKey를 생성하고
    JWT 헤더, 클레임, 만료 시간 등을 설정하여 최종적으로 JWT 토큰을 생성

---
+++ backend/src/main/java/com/example/backend/service/JwtServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.backend.service;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtServiceImpl implements JwtService {
+
+    private String secretKey = "asjdk!1256kdjlrk$#@fjdkli$fdskfjlwepcmv@#53345sjyt";
+
+    @Override
+    public String getToken(String key, Object value) {
+
+        Date expTime = new Date();
+        expTime.setTime(expTime.getTime() + 1000 * 60 * 30); //30분짜리
+
+        String base64EncodedSecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        byte[] secretByteKey = Base64.getDecoder().decode(base64EncodedSecretKey);
+        Key signKey = new SecretKeySpec(secretByteKey, SignatureAlgorithm.HS256.getJcaName());
+
+        Map<String, Object> headerMap = new HashMap<>();
+        headerMap.put("typ", "JWT");
+        headerMap.put("alg", "HS256");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put(key, value);
+
+        JwtBuilder builder = Jwts.builder().setHeader(headerMap)
+                .setClaims(map)
+                .setExpiration(expTime)
+                .signWith(signKey, SignatureAlgorithm.HS256);
+
+        return builder.compact();
+    }
+}
