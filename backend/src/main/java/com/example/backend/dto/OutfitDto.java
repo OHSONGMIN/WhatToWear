2e92b7f30133ff99e9ea98ff0ccbec2d510ebceb
OHSONGMIN <dhthdals@gmail.com>
2024-05-27 18:00:55 +0900

    feat: OutfitDto 생성

---
+++ backend/src/main/java/com/example/backend/dto/OutfitDto.java
@@ -0,0 +1,13 @@
+package com.example.backend.dto;
+
+import lombok.Getter;
+
+@Getter
+public class OutfitDto {
+
+    private int overcoat;
+    private int top;
+    private int bottom;
+    private int accessory;
+    private String review;
+}
