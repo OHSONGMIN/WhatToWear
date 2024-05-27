d4eaf70a3a80425e160b19b23fce77d7ce3f0609
OHSONGMIN <dhthdals@gmail.com>
2024-05-27 18:01:09 +0900

    feat: Outfit Entity 생성

---
+++ backend/src/main/java/com/example/backend/entity/Outfit.java
@@ -0,0 +1,45 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "outfits")
+public class Outfit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private int memberId;
+
+    @Column(nullable = false)
+    private LocalDateTime regdate;
+
+    @Column(length = 100, nullable = false)
+    private String review;
+
+    @Column
+    private int overcoat;
+
+    @Column
+    private int top;
+
+    @Column
+    private int bottom;
+
+    @Column
+    private int accessory;
+
+    @Column(length = 20)
+    private String region;
+
+    @Column
+    private int temperature;
+}
