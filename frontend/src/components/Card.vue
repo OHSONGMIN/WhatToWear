929c6827ce2416381a667d5ea42ac33394521dbe
OHSONGMIN <dhthdals@gmail.com>
2024-05-28 11:49:17 +0900

    feat: 전달받은 Outfits을 출력

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,27 +1,26 @@
 <template>
-  Card........
-  {{item.name}}
-  <button class="btn btn-primary" @click="addToCart(item.id)">
-    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-  </button>
+  {{item.regdate}}
+  {{item.overcoat}}
+  {{item.top}}
+  {{item.bottom}}
+  {{item.accessory}}
+  {{item.review}}
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
   name: "Card",
   props: {
     item: Object
   },
   setup() {
-    const addToCart = (itemdId) => {
-      axios.post(`/api/cart/items/${itemdId}`).then(() => {
-        console.log("success");
-      })
-    };
-
-    return {addToCart}
+    // const addToCart = (itemdId) => {
+    //   axios.post(`/api/cart/items/${itemdId}`).then(() => {
+    //     console.log("success");
+    //   })
+    // };
+    //
+    // return {addToCart}
   }
 }
 </script>
