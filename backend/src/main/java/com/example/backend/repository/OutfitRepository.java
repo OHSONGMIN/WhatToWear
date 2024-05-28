1bcd40f6eece94f1ef1e7f36dab915fa6a054f10
OHSONGMIN <dhthdals@gmail.com>
2024-05-28 15:28:47 +0900

    chore: delStatus가 0인 outfits만 불러오는 기능

--- backend/src/main/java/com/example/backend/repository/OutfitRepository.java
+++ backend/src/main/java/com/example/backend/repository/OutfitRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.repository;
 
 import com.example.backend.entity.Outfit;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,6 +9,8 @@ import java.util.List;
 
 public interface OutfitRepository extends JpaRepository<Outfit, Integer> {
 
-    @Query("SELECT o FROM Outfit o ORDER BY o.id DESC")
-    List<Outfit> findAllOrderByIdDesc();
+    @Query("SELECT o FROM Outfit o WHERE o.delStatus = 0 ORDER BY o.id DESC")
+    List<Outfit> findOutfits();
+
+    //void updateDelStatus(int outfitId, int i);
 }
