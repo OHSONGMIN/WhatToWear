d84749d11dca050bda38bae55cc7f5051cda9345
OHSONGMIN <dhthdals@gmail.com>
2024-05-28 16:44:38 +0900

    feat: 모달찰 관리하는 기능 추가

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -3,17 +3,19 @@
     <Card :outfit="outfit" @deleted="load"/> <!-- outfit이라는 이름으로 outfit객체를 전달-->
     <!-- Card 컴포넌트에서 발생한 deleted 이벤트를 수신 -->
   </div>
-  <router-link to="/write" class="btn btn-primary">작성하기</router-link>
+  <Write v-if="modalStatus" @sendClose="closeModal" @sendLoad="load"></Write>
+  <button type="button" @click="openModal">작성하기</button>
 </template>
 
 <script>
 import axios from "axios";
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 import Card from "@/components/Card.vue";
+import Write from "@/components/Write.vue";
 
 export default {
   name: "Home",
-  components: {Card},
+  components: {Card, Write},
   setup() {
     const state = reactive({
       outfits: []
@@ -27,7 +29,17 @@ export default {
 
     load();
 
-    return {state, load}
+    const modalStatus = ref(false); //반응형
+
+    const openModal = () => {
+      modalStatus.value = true;
+    }
+
+    const closeModal = () => {
+      modalStatus.value = false;
+    }
+
+    return {state, load, modalStatus, openModal, closeModal}
   }
 }
 </script>
