042b821ea0c2924d53f9edf4fdba8f52524ebaf3
OHSONGMIN <dhthdals@gmail.com>
2024-06-01 17:26:35 +0900

    feat: 날씨 정보 콘솔에 출력

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -23,8 +23,8 @@ export default {
     const state = reactive({
       outfits: [],
       address: {
-        borough: "", //신봉동
-        city: "", //용인시
+        //borough: "", //신봉동
+        //city: "", //용인시
       },
       // grid: {
       //   gridX: "",
@@ -52,10 +52,24 @@ export default {
           state.address = data.address;
         })
 
-        axios.get(`/api/grid?lat=${lat}&lon=${lon}`).then(() => {
-          //console.log("res.data.x= " + data.x + "res.data.y= " + data.y); //찍힘!!
-          // state.grid.gridX = data.x;
-          // state.grid.gridY = data.y;
+        axios.get(`/api/weather?lat=${lat}&lon=${lon}`).then((res) => {
+          /*
+           * POP	강수확률	% ★
+           * PTY	강수형태	코드값 ★
+           * PCP	1시간 강수량	범주 (1 mm) ★
+           * REH	습도	% ★
+           * SNO	1시간 신적설	범주(1 cm) ★
+           * SKY	하늘상태	코드값 ★
+           * TMP	1시간 기온	℃ ★
+           * TMN	일 최저기온	℃
+           * TMX	일 최고기온	℃
+           * UUU	풍속(동서성분)	m/s ★
+           * VVV	풍속(남북성분)	m/s ★
+           * WAV	파고	M ★
+           * VEC	풍향	deg ★
+           * WSD	풍속	m/s ★
+           */
+          console.log("날씨는 " + JSON.stringify(res.data));
         })
       })
     };
