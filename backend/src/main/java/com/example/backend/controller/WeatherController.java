3231ada2f1e8f7709c52817f54358013d04fbf2d
OHSONGMIN <dhthdals@gmail.com>
2024-06-05 14:57:06 +0900

    feat: 캐시가 존재하면 캐시에서 데이터를 불러오는 기능 + HTTP 헤더 설정 + API url 직접 인코딩

--- backend/src/main/java/com/example/backend/controller/WeatherController.java
+++ backend/src/main/java/com/example/backend/controller/WeatherController.java
@@ -3,34 +3,32 @@ package com.example.backend.controller;
 
 import com.example.backend.service.GridService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
-import java.net.*;
-import java.nio.charset.Charset;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
 @RestController
 public class WeatherController {
 
     private String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
-    //private String apiKey = "B7yvNvRj2awYzuewTUeXeYA3QUT5q19gCrX3sIlZrKQtv%2Bc7HhloJDeHQnlWN8%2BeA7VPqaHYiQ7ZlrmuzZ0OEQ%3D%3D"; //encoding
-    private String apiKey = "B7yvNvRj2awYzuewTUeXeYA3QUT5q19gCrX3sIlZrKQtv+c7HhloJDeHQnlWN8+eA7VPqaHYiQ7ZlrmuzZ0OEQ=="; //decoding
+    private String apiKey = ""; //encoding
 
     @Autowired
     private GridService gridService;
 
+    @Autowired
+    private CacheManager cacheManager;
+
     @GetMapping("/api/weather")
-    public ResponseEntity getWeather(
+    public ResponseEntity<String> getWeather(
             @RequestParam double lat, @RequestParam double lon) {
 
         //위도, 경도 -> 격자(x, y)
@@ -44,83 +42,74 @@ public class WeatherController {
         String baseDate = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         String baseTime = timeChange(time.format(DateTimeFormatter.ofPattern("HH00")));
 
+        //캐시 데이더
+        String cacheKey = baseDate + baseTime + "-" + lat + "-" + lon;
+        String cachedData = getCachedWeatherData(cacheKey);
+
+        if (cachedData != null) {
+            return new ResponseEntity<>(cachedData, HttpStatus.OK);
+        }
         String dataType = "JSON";
+
+        /*
+        String url = String.format("%s?serviceKey=%s&numOfRows=290&pageNo=1&base_date=%s&base_time=%s&nx=%d&ny=%d",
+                apiUrl, apiKey, baseDate, baseTime, x.intValue(), y.intValue());
+                //RestTemplate을 사용해 API 호출
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        */
+
+        //API 요청
         String encodedApiKey = URLEncoder.encode(apiKey, StandardCharsets.UTF_8);
         String url = String.format("%s?serviceKey=%s&numOfRows=290&pageNo=1&base_date=%s&base_time=%s&nx=%.0f&ny=%.0f&dataType=%s", apiUrl, encodedApiKey, baseDate, baseTime, x, y, dataType);
 
         System.out.println("Request URL: " + url); // 로그에 요청 URL 출력
 
+        // HTTP 헤더 설정
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // RestTemplate을 사용해 API 요청 수행
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
         System.out.println("Response: " + response);
-        System.out.println("Request URL: " + url);
-        System.out.println("Response Status Code: " + response.getStatusCode());
-        System.out.println("Response Headers: " + response.getHeaders());
-        System.out.println("Response Body: " + response.getBody());
 
         if (response.getStatusCode() == HttpStatus.OK) {
-            return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+
+            String responseBody = response.getBody();
+
+            if (responseBody != null && !responseBody.contains("SERVICE_KEY_IS_NOT_REGISTERED_ERROR")) {
+                cacheWeatherData(cacheKey, responseBody);
+                System.out.println("캐시 데이터..!!");
+                return new ResponseEntity<>(responseBody, HttpStatus.OK);
+
+            }
+            else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("등록되지 않은 서비스 키입니다리롱");
+            }
         } else {
-            return ResponseEntity.status(response.getStatusCode()).body("Failed to fetch weather data");
+            return ResponseEntity.status(response.getStatusCode()).body("날씨 정보를 불러오지 못했다리");
         }
-/*
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+    }
 
-        UriComponents uriBuilder = UriComponentsBuilder.fromHttpUrl(apiUrl)
-                .queryParam("serviceKey", apiKey)
-                .queryParam("numOfRows", 290)
-                .queryParam("pageNo", 1)
-                .queryParam("base_date", baseDate)
-                .queryParam("base_time", baseTime)
-                .queryParam("nx", x.intValue())
-                .queryParam("ny", y.intValue())
-                .queryParam("dataType", "JSON")
-                .build();
+    private void cacheWeatherData(String cacheKey, String body) {
+        cacheManager.getCache("weatherCache").put(cacheKey, body);
+    }
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(uriBuilder.toUriString(), HttpMethod.GET, new HttpEntity<String>(headers), String.class);
-        System.out.println("Request URL: " + uriBuilder);
-*/
-
-        //API 요청
-        //String url = String.format("%s?serviceKey=%s&numOfRows=290&pageNo=1&base_date=%s&base_time=%s&nx=%.0f&ny=%.0f&dataType=%s", apiUrl, apiKey, baseDate, baseTime, x, y, dataType);
-
-        /*
-        String url = String.format("%s?serviceKey=%s&numOfRows=290&pageNo=1&base_date=%s&base_time=%s&nx=%d&ny=%d",
-                apiUrl, apiKey, baseDate, baseTime, x.intValue(), y.intValue());
-*/
-
-  /*      System.out.println("Request URL: " + url); // 로그에 요청 URL 출력
-
-
-        //RestTemplate을 사용해 API 호출
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-        System.out.println("Response: " + response);
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } else {
-            return ResponseEntity.status(response.getStatusCode()).body("Failed to fetch weather data");
-        }
-
-   */
+    private String getCachedWeatherData(String cacheKey) {
+        return cacheManager.getCache("weatherCache").get(cacheKey, String.class);
     }
 
     public String timeChange(String time) {
-        /**
+        /*
          시간은 3시간 단위로 조회해야 한다. 안그러면 정보가 없다고 뜬다.
          0200, 0500, 0800 ~ 2300까지
          그래서 시간을 입력했을때 switch문으로 조회 가능한 시간대로 변경해주었다.
-         **/
+         */
 
         switch (time) {
 
