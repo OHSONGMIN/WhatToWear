4d73bff5c20ca34220152587e1fe9b0bac8e4fc5
OHSONGMIN <dhthdals@gmail.com>
2024-07-02 12:16:19 +0900

    chore: SKY 상태 조건 구분

--- frontend/src/components/Weather.vue
+++ frontend/src/components/Weather.vue
@@ -5,7 +5,11 @@
       <p>최저 기온: {{ weatherData.minTemp }}</p>
       <p>최고 기온: {{ weatherData.maxTemp }}</p>
       <p>강수 확률: {{ weatherData.pop }}</p>
-      <p>하늘 상태: {{ weatherData.sky }}</p>
+      <div>
+      <p v-if="weatherData.sky <= 5"> 맑음</p>
+      <p v-else-if="weatherData.sky <= 8"> 구름많음</p>
+      <p v-else> 흐림</p>
+      </div>
     </div>
 
     <div v-if="address">
