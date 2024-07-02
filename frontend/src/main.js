869ca3d1d983d26ed8580417fd55c0c0564047fb
OHSONGMIN <dhthdals@gmail.com>
2024-07-02 16:26:14 +0900

    feat: import bootstrap icon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import store from "@/scripts/store"
 import router from "@/scripts/router";
 import App from './App.vue'
 
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createApp(App).use(store).use(router).mount('#app')
