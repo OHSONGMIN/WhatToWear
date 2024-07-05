b0b53e03278ba8c0f192eeb07c54c1ca3363efde
OHSONGMIN <dhthdals@gmail.com>
2024-07-05 17:27:25 +0900

    feat: 작성내역 확인 가능한 History.vue 생성

---
+++ frontend/src/pages/History.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  주문내역.....!!
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import {reactive} from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      history: [],
+    })
+
+    axios.get("/api/history").then(({data}) => {
+      state.history = [];
+
+      for (let d of data) {
+        if(d.items) {
+          d.items = JSON.parse(d.items);
+        }
+        state.history.push(d);
+      }
+    })
+
+    return {state};
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
