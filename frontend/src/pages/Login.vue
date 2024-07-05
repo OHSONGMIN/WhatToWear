4eae93fc66abcfc6eb98ec88520162f951181c81
OHSONGMIN <dhthdals@gmail.com>
2024-07-05 17:26:25 +0900

    css: Login.vue css 수정

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="form-signin w-100 m-auto">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+  <div class="container">
+    <div class="form-signin w-100 m-auto">
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
-             @keyup.enter="submit()"
-             v-model="state.form.email">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
-             @keyup.enter="submit()"
-             v-model="state.form.password">
-      <label for="floatingPassword">Password</label>
-    </div>
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+               @keyup.enter="submit()"
+               v-model="state.form.email">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+               @keyup.enter="submit()"
+               v-model="state.form.password">
+        <label for="floatingPassword">Password</label>
+      </div>
 
-    <button class="btn btn-primary w-100 py-2" @click="submit()">Sign in</button>
-    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+      <button class="btn btn-primary w-100 py-2 login-button" @click="submit()">Sign in</button>
+    </div>
   </div>
 </template>
 
@@ -38,7 +39,7 @@ export default {
     const submit = () => {
       axios.post("/api/account/login", state.form).then((res) => {
         store.commit('setAccount', res.data); //로그인 하고 받은 id 값을 store에 저장하겠다.
-        sessionStorage.setItem("id", res.data);
+        //sessionStorage.setItem("id", res.data); //지워도 될 듯... sessionStorage 사용하지 않으니까
         router.push({path: "/"});
         window.alert("로그인하였습니다.");
       }).catch(() => {
@@ -52,9 +53,18 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* min-height: 400px; */
+  height: 70vh;
+}
+
 .form-signin {
   max-width: 330px;
   padding: 1rem;
+  width: 100%;
 }
 
 .form-signin .form-floating:focus-within {
@@ -71,5 +81,13 @@ export default {
   margin-bottom: 10px;
   border-top-left-radius: 0;
   border-top-right-radius: 0;
+}
+
+.login-button {
+  background-color: #635E4E;
+}
+
+.login-button:hover {
+  background-color: #B0AB99;
 }
 </style>
