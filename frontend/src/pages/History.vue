39f8443795c9eebcc19d902507ebc259aacdae0f
OHSONGMIN <dhthdals@gmail.com>
2024-07-06 16:51:05 +0900

    feat: 작성 내역 확인할 수 있는 History.vue 생성

--- frontend/src/pages/History.vue
+++ frontend/src/pages/History.vue
@@ -1,29 +1,42 @@
 <template>
-<div>
-  주문내역.....!!
-</div>
+  <div>
+    작성 내역<br>
+    <div class="col" v-for="outfit in state.history" :key="outfit.id">
+      <Card :outfit="outfit" @deleted="load"/>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import {reactive} from "vue";
+import Card from "@/components/Card.vue";
 
 export default {
+  components: {Card},
   setup() {
     const state = reactive({
       history: [],
     })
 
-    axios.get("/api/history").then(({data}) => {
-      state.history = [];
+    const load = () => {
+      axios.get("/api/history").then((res) => {
+        state.history = res.data;
+      })
+    }
 
-      for (let d of data) {
-        if(d.items) {
-          d.items = JSON.parse(d.items);
-        }
-        state.history.push(d);
-      }
-    })
+    load();
+
+    // axios.get("/api/history").then(({data}) => {
+    //   state.history = [];
+    //
+    //   for (let d of data) {
+    //     if(d.items) {
+    //       d.items = JSON.parse(d.items);
+    //     }
+    //     state.history.push(d);
+    //   }
+    // })
 
     return {state};
   }
