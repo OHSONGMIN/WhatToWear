94054cbdd8f19507a241116a4f5d5bf4a9594c9f
OHSONGMIN <dhthdals@gmail.com>
2024-07-06 16:50:26 +0900

    feat: 내 작성 목록 가져오는 기능 추가

--- backend/src/main/java/com/example/backend/repository/OutfitRepository.java
+++ backend/src/main/java/com/example/backend/repository/OutfitRepository.java
@@ -1,7 +1,6 @@
 package com.example.backend.repository;
 
 import com.example.backend.entity.Outfit;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,6 +10,11 @@ public interface OutfitRepository extends JpaRepository<Outfit, Integer> {
 
     @Query("SELECT o FROM Outfit o WHERE o.delStatus = 0 ORDER BY o.id DESC")
     List<Outfit> findOutfits();
+
+    List<Outfit> findByMemberIdOrderByIdDesc(int memberId);
+
+//    @Query("SELECT o FROM Outfit o JOIN FETCH o.details d JOIN FETCH d.item")
+//    List<Outfit> findOutfitsWithDetails();
 
     //void updateDelStatus(int outfitId, int i);
 }
