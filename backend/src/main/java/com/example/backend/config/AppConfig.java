7fae8823b16bde8eb50b423b1344ef6a75ab499c
OHSONGMIN <dhthdals@gmail.com>
2024-07-11 14:09:20 +0900

    feat: 비밀번호 암호화 하기 위한 AppConfig 생성

---
+++ backend/src/main/java/com/example/backend/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+    /*
+    명시적으로 BcryptPasswordEncoder를 등록하지 않고
+    PasswordEncoder를 등록한 이유는
+    디폴트 PasswordEncoder가 Bcrypt + salt를 사용하기 때문
+     */
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
