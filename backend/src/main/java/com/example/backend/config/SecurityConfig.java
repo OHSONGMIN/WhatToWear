a6c4d951f2ed1a3e09faab00033a8fec69c9fd77
OHSONGMIN <dhthdals@gmail.com>
2024-07-26 21:31:44 +0900

    fix: 401 에러 해결

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.example.backend.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,14 +14,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConsig {
+public class SecurityConfig {
 
     @Bean
     @ConditionalOnMissingBean(UserDetailsService.class)
-    InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         String generatedPassword = "sajdklj3483okrldsjfklasdjf";
         return new InMemoryUserDetailsManager(User.withUsername("user")
                 .password(generatedPassword).roles("USER").build());
@@ -38,14 +40,20 @@ public class SecurityConsig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(AbstractHttpConfigurer::disable
-                )
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/api/history").authenticated()
+                                .requestMatchers("/api/items").authenticated()
+                                //.anyRequest().permitAll()
                                 .requestMatchers("/**").permitAll()
+                                //.anyRequest().permitAll()
+                        //jwtService 소환하는 것들 다..... requestMAtchers 해야하나?
                 );
 
         return http.build();
     }
+
+
 }
