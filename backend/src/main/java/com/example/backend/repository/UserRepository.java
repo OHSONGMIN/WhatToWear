56a0afeeb1d0c80bb53009a18f7a8cd5957a9e78
OHSONGMIN <dhthdals@gmail.com>
2024-07-30 22:19:20 +0900

    feat: UserRepository 생성

---
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Member, Integer> {
+
+    Boolean existByEmail(String email);
+}
