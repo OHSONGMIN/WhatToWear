acb31a8378a32a8ab3851d06e0a3661eac1c0ebc
OHSONGMIN <dhthdals@gmail.com>
2024-08-07 23:52:06 +0900

    feat: clearAccount, setAuthToken, clearAuthToken

--- frontend/src/scripts/store.js
+++ frontend/src/scripts/store.js
@@ -5,12 +5,24 @@ const store = createStore({
         return {
             account: {
                 id: 0
-            }
+            },
+            authToken: localStorage.getItem(`authToken`) || null
         }
     },
     mutations: {
         setAccount(state, payload) {
             state.account.id = payload;
+        },
+        clearAccount(state) {
+            state.account.id = 0;
+        },
+        setAuthToken(state, token) {
+            state.authToken = token;
+            localStorage.setItem(`authToken`, token);
+        },
+        clearAuthToken(state) {
+            state.authToken = null;
+            localStorage.removeItem(`authToken`);
         }
     }
 })
