3e3eb944731a2c11f107a32e98ff0902a36896cc
OHSONGMIN <dhthdals@gmail.com>
2024-08-09 23:40:18 +0900

    chore: Cookie에서 id 추출 후 store에 저장

--- frontend/src/scripts/store.js
+++ frontend/src/scripts/store.js
@@ -1,10 +1,11 @@
 import {createStore} from 'vuex'
+import Cookies from 'js-cookie'
 
 const store = createStore({
     state() {
         return {
             account: {
-                id: 0
+                id: Cookies.get(`id`) || 0
             },
             authToken: localStorage.getItem(`authToken`) || null
         }
@@ -12,9 +13,12 @@ const store = createStore({
     mutations: {
         setAccount(state, payload) {
             state.account.id = payload;
+            Cookies.set(`id`, payload, {expires: 7})
+            console.log(state.account.id + "넘어왔나요?")
         },
         clearAccount(state) {
             state.account.id = 0;
+            Cookies.remove(`id`);
         },
         setAuthToken(state, token) {
             state.authToken = token;
