75125397c18dbf166f57f0a9f36ca7c423965a75
OHSONGMIN <dhthdals@gmail.com>
2024-08-13 23:34:51 +0900

    feat: Refresh Entity 생성

---
+++ backend/src/main/java/com/example/backend/entity/Refresh.java
@@ -0,0 +1,24 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Refresh {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 1000)
+    private String refresh;
+
+    @Column(length = 100)
+    private String expiration;
+}
