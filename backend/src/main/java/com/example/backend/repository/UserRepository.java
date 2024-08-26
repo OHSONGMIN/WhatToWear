cb409e96a6e0e510b65840481a418abaf6ebc0b3
OHSONGMIN <dhthdals@gmail.com>
2024-08-26 23:02:44 +0900

    feat: email을 받아서 DB에서 회원 조회하는 기능 추가

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<Member, Integer> {
 
     Boolean existsByEmail(String email);
+
+    //email을 받아 DB 테이블에서 회원을 조회
+    Member findByEmail(String email);
 }
