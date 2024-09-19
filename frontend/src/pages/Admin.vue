fba77aed93001c10ffe977699bb8f0dcd7c6defe
OHSONGMIN <dhthdals@gmail.com>
2024-09-19 16:49:24 +0900

    feat: 관리자 페이지 Admin.vue

---
+++ frontend/src/pages/Admin.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="admin-container">
+    <h2>관리자 페이지</h2>
+    <div class="admin-actions">
+      <router-link class="btn btn-primary" to="/adminMember">회원 관리</router-link>
+      <router-link class="btn btn-primary" to="/adminOutfit">리뷰 관리</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Admin",
+
+}
+</script>
+
+<style scoped>
+.admin-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+
+.btn-primary {
+  display: block;
+  padding: 10px 20px;
+  font-size: 18px;
+  cursor: pointer;
+  background-color: #B0AB99;
+  border: none;
+  margin: 20px;
+}
+</style>
