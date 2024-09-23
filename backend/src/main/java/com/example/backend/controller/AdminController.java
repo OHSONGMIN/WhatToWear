2c3c871be95d81b3b3c7a1c94eeed39621703784
OHSONGMIN <dhthdals@gmail.com>
2024-09-23 21:49:53 +0900

    feat: AdminController 생성 및 "/api/admin/searchMember" 로직 추가

--- backend/src/main/java/com/example/backend/controller/AdminController.java
+++ backend/src/main/java/com/example/backend/controller/AdminController.java
@@ -1,16 +1,35 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.SearchKeywordDto;
+import com.example.backend.entity.Member;
+import com.example.backend.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @ResponseBody
 public class AdminController {
 
-    @GetMapping("/adminP")
-    public String adminP() {
+    @Autowired
+    private AdminRepository adminRepository;
 
-        return "admin Controller";
+    @PostMapping("/api/admin/searchMember")
+    public ResponseEntity searchMember(
+            @RequestBody SearchKeywordDto dto
+            ) {
+
+        System.out.println("도착??");
+        List<Member> members = adminRepository.findByEmail(dto.getKeyword());
+
+        return new ResponseEntity<>(members, HttpStatus.OK);
     }
+
 }
