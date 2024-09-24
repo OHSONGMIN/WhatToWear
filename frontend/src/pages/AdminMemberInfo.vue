db6e218542f73476234e31178c79297ac61331a4
OHSONGMIN <dhthdals@gmail.com>
2024-09-24 14:56:19 +0900

    feat: 회원 상세 정보 관리 페이지 생성

---
+++ frontend/src/pages/AdminMemberInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="admin-container">
+    <h2>회원 정보 관리</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdminMemberInfo",
+
+}
+</script>
+
+<style scoped>
+
+</style>
