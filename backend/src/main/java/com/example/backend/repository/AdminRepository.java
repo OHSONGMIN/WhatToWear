f644a4aa2d8f4efa060812d75482645c504e3a5a
OHSONGMIN <dhthdals@gmail.com>
2024-09-24 16:09:40 +0900

    feat: findByEmail, findByKeyword

--- backend/src/main/java/com/example/backend/repository/AdminRepository.java
+++ backend/src/main/java/com/example/backend/repository/AdminRepository.java
@@ -1,15 +1,16 @@
 package com.example.backend.repository;
 
 import com.example.backend.entity.Member;
-import com.example.backend.entity.Outfit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AdminRepository extends JpaRepository<Member, Integer> {
 
     @Query("SELECT m from Member m WHERE m.email LIKE %:keyword%")
-    List<Member> findByEmail(String keyword);
+    List<Member> findByKeyword(@Param("keyword") String keyword);
 
+    Member findByEmail(String email);
 }
