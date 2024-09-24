65f62d3bc03b48358e4524b2eb58225f76c50816
OHSONGMIN <dhthdals@gmail.com>
2024-09-24 13:05:30 +0900

    chore: api 주소 및 param 받는 방법 변경

--- backend/src/main/java/com/example/backend/controller/AdminController.java
+++ backend/src/main/java/com/example/backend/controller/AdminController.java
@@ -1,33 +1,26 @@
 package com.example.backend.controller;
 
-import com.example.backend.dto.SearchKeywordDto;
 import com.example.backend.entity.Member;
 import com.example.backend.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@ResponseBody
+@RestController
 public class AdminController {
 
     @Autowired
     private AdminRepository adminRepository;
 
-    @PostMapping("/api/admin/searchMember")
+    @GetMapping("/api/admin/searchMember")
     public ResponseEntity searchMember(
-            @RequestBody SearchKeywordDto dto
+            @RequestParam("email") String email
             ) {
 
-        System.out.println("도착??");
-        List<Member> members = adminRepository.findByEmail(dto.getKeyword());
+        List<Member> members = adminRepository.findByEmail(email);
 
         return new ResponseEntity<>(members, HttpStatus.OK);
     }
