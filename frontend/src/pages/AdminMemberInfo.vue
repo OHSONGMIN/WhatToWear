17a8cd5479ed635bf3d7bfad129f65728ffdb0ff
OHSONGMIN <dhthdals@gmail.com>
2024-09-25 21:39:30 +0900

    chore: 삭제 버튼 누르면 다시 리뷰 목록을 불러오도록 수정

--- frontend/src/pages/AdminMemberInfo.vue
+++ frontend/src/pages/AdminMemberInfo.vue
@@ -13,12 +13,19 @@
         </tr>
         <tr>
           <td>탈퇴여부</td>
-          <td>
-            {{ state.member.delStatus }}
-            <button @click="forcedDelete()">강제 탈퇴</button>
+          <td class="withdrawBtn">
+            {{ state.member.delStatus === 0 ? state.member.delStatus : "탈퇴" }}
+            <button @click="adminWithdraw(state.member.email)">강제 탈퇴</button>
           </td>
         </tr>
       </table>
+    </div>
+
+    <button @click="getMemberOutfits(state.member.email)">작성한 리뷰 목록</button>
+  </div>
+  <div v-if="state.outfits">
+    <div class="col" v-for="outfit in state.outfits" :key="outfit.id">
+      <Card :outfit="outfit" @deleted="getMemberOutfits(state.member.email)"/>
     </div>
   </div>
 </template>
@@ -27,13 +34,16 @@
 import {useRoute} from "vue-router";
 import axios from "axios";
 import {reactive} from "vue";
+import Card from "@/components/Card.vue";
 
 export default {
   name: "AdminMemberInfo",
+  components: {Card},
   setup() {
     const route = useRoute();
     const state = reactive({
       member: "",
+      outfits: [],
     })
 
     // 이메일로 회원 상세 정보 조회
@@ -51,12 +61,42 @@ export default {
 
     getMemberInfo(route.params.email);
 
-    const forcedDelete = () => {
-      alert("탈퇴할래?");
+    const adminWithdraw = (email) => {
+      const confirmed = confirm("강제로 탈퇴시키겠습니까?");
 
+      if (confirmed) {
+
+        if (state.member.delStatus === 0) {
+          axios.patch(`/api/admin/withdraw/${email}`)
+              .then(() => {
+                alert("탈퇴 완료");
+
+                //업데이트
+                state.member.delStatus = 1;
+              })
+              .catch((error) => {
+                console.error("탈퇴 실패: " + error);
+              })
+        } else {
+          alert("이미 탈퇴된 회원입니다.")
+        }
+      } else {
+        alert("취소되었습니다.");
+      }
     }
 
-    return { state, forcedDelete };
+    const getMemberOutfits = (email) => {
+      axios.get(`/api/admin/getOutfits/${email}`)
+          .then((res) => {
+            state.outfits = res.data;
+            console.log(res);
+          })
+          .catch((error) => {
+            console.error("리뷰 목록을 불러오지 못했습니다: ", error);
+          })
+    }
+
+    return {state, adminWithdraw, getMemberOutfits};
   }
 }
 </script>
@@ -83,5 +123,11 @@ table {
 td {
   padding: 5px 5px;
   border-right: 1px solid #D5D5D5;
+}
+
+.withdrawBtn {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
