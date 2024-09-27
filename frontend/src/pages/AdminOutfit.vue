731705de0487c910b99e2980dde6fcd3946513b2
OHSONGMIN <dhthdals@gmail.com>
2024-09-27 23:27:30 +0900

    feat: 기간 범위 설정 위한 캘린더 추가

--- frontend/src/pages/AdminOutfit.vue
+++ frontend/src/pages/AdminOutfit.vue
@@ -1,37 +1,122 @@
 <template>
-  <div class="col" v-for="outfit in state.outfits" :key="outfit.id">
-    <Card :outfit="outfit" @deleted="load"/>
-    <!-- Card 컴포넌트에서 발생한 deleted 이벤트를 수신 -->
+  <div class="admin-container">
+    <h2>리뷰 관리</h2><br>
+    <h6>조회 기간을 선택하세요.</h6>
+    <div>
+
+      <VueDatePicker locale="ko" v-model="date" />
+    </div>
+    <div>
+      <form>
+        <input type="search"
+               placeholder="이메일을 입력하세요"
+               v-model="state.email"
+        />
+        <button @click.prevent="search()">검색</button>
+      </form>
+    </div>
+
+    <!-- 검색어가 있으면 -->
+    <div v-if="state.searchKeyword" class="searchResult">
+      <h6>"{{ state.searchKeyword }}"로 검색한 결과입니다</h6>
+
+      <!-- 검색 결과가 1개 이상 -->
+      <table v-if="state.members.length > 0" class="styled-table">
+        <thead>
+        <tr>
+          <th>이메일</th>
+          <th>탈퇴여부</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="member in state.members" :key="member.id">
+          <td>{{ member.email }}</td>
+          <td>{{ member.delStatus }}</td>
+        </tr>
+        </tbody>
+      </table>
+
+      <!-- 검색 결과가 0개 -->
+      <p v-else>검색 결과가 존재하지 않습니다.</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive } from "vue";
 import axios from "axios";
-import {reactive} from "vue";
-import Card from "@/components/Card.vue";
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 
 export default {
   name: "AdminOutfit",
-  components: {Card},
-
+  components: {
+    VueDatePicker
+  },
   setup() {
     const state = reactive({
-      outfits: [],
+      email: "",
+      members: [],
+      searchKeyword: ""
     })
 
-    const load = () => {
-      axios.get("/api/main/outfits").then((res) => {
-        state.outfits = res.data;
+    const search = () => {
+      axios.get("/api/admin/searchMember", {
+        params: { email: state.email }
       })
+          .then((res) => {
+            state.members = res.data;
+            state.searchKeyword = state.email;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
     }
-
-    load();
-
-    return {state, load}
+    return { state, search };
   }
 }
 </script>
 
 <style scoped>
+.admin-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
 
+form {
+  display: block;
+  margin: 20px;
+}
+
+button {
+  cursor: pointer;
+}
+
+p {
+  font-size: 20px;
+  margin: 20px;
+}
+
+.searchResult {
+  text-align: center;
+}
+
+table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  border: 1px solid #D5D5D5;
+  min-width: 300px;
+}
+
+.styled-table thead tr {
+  background-color: #D5D5D5;
+  font-size: 15px;
+}
+
+.styled-table th, .styled-table td {
+  padding: 10px 5px;
+  border-right: 1px solid #D5D5D5;
+}
 </style>
