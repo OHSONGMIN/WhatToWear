603796f923850708cf343484855525fce0489960
OHSONGMIN <dhthdals@gmail.com>
2024-09-30 21:49:50 +0900

    feat: 기간에 맞는 Outfit 불러오는 findOutfitsRange 추가

--- backend/src/main/java/com/example/backend/repository/OutfitRepository.java
+++ backend/src/main/java/com/example/backend/repository/OutfitRepository.java
@@ -4,6 +4,7 @@ import com.example.backend.entity.Outfit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,4 +20,7 @@ public interface OutfitRepository extends JpaRepository<Outfit, Integer> {
     List<Outfit> findAllOutfitsByMemberId(int memberId);
 
     Outfit findById(int id);
+
+    @Query("SELECT o FROM Outfit o WHERE o.regdate BETWEEN :startDate AND :endDate ORDER BY o.id DESC")
+    List<Outfit> findOutfitsRange(LocalDateTime startDate, LocalDateTime endDate);
 }
