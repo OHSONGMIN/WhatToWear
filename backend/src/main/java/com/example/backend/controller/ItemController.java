fa072a328b614f67eaaca9b62dd797a617984c7d
OHSONGMIN <dhthdals@gmail.com>
2024-11-06 18:17:56 +0900

    chore: active한 item만 불러오도록 수정

--- backend/src/main/java/com/example/backend/controller/ItemController.java
+++ backend/src/main/java/com/example/backend/controller/ItemController.java
@@ -2,14 +2,11 @@ package com.example.backend.controller;
 
 import com.example.backend.entity.Item;
 import com.example.backend.repository.ItemRepository;
-import com.example.backend.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -17,22 +14,12 @@ import java.util.List;
 public class ItemController {
 
     @Autowired
-    JwtService jwtService;
-
-    @Autowired
     ItemRepository itemRepository;
 
     @GetMapping("/api/outfit/items")
-    public ResponseEntity getItems(
-//            @CookieValue(value = "token", required = true) String token
-    ) {
-//        System.out.println("token : " + token);
-//
-//        if (!jwtService.isValid(token)) {
-//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-//        }
+    public ResponseEntity getItems() {
 
-        List<Item> items = itemRepository.findAll();
+        List<Item> items = itemRepository.findActiveItem();
 
         return new ResponseEntity<>(items, HttpStatus.OK);
     }
